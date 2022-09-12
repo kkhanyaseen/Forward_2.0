32d4d071b104eb2713f89a12231fe1787ac65703
『ᴘɪʀᴏ』 <96607700+ritheshrkrm17@users.noreply.github.com>
2022-09-12 19:49:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 import logging
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = int(os.environ.get("API_ID", "15430744"))
+    API_HASH = os.environ.get("API_HASH", "5723974576:AAFsxorOuj2iwEqgwA-UuBwyjBmGJK-jTFk")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5723974576:AAFsxorOuj2iwEqgwA-UuBwyjBmGJK-jTFk") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
-    OWNER_ID = os.environ.get("OWNER_ID", "")
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+    OWNER_ID = os.environ.get("OWNER_ID", "761686219")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://eva:eva@cluster0.wn73m.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
-    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
-    SESSION = os.environ.get("SESSION", "Forward_Session")
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
+    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'workflow')
+    SESSION = os.environ.get("SESSION", "BQBk-G3hHTDytHtDroeAAcU20XIrVhh0BV3iuKYpidM2JbMGNTWc8qLl0xXa6fzdfYdYlBYAaDZ0SFVJkb_Uv7vf7qiHUXmJiAtrT5Kc3WDQrN3x-qfruIb6BYTW7E2q9U7S7uSBKfbVFqV20u1qTpoD3mAU8dg_TWX7GsraAJVY30Pe70AN-NkDw9yBr8yax06EQPv80tjTNazZgEb-inHPCkcl_WuGnDD8dCRVto7fhyh-HrMLJFNpk9jSKwUYlr3wZgiJzB07XhNE1U2jEtvUjm1CZf-QCD1L2lyYgZr-mlLg5bcRUKqXfeXxlZ-9ErpTrgdM9uDo0SdyLQFKpLWHUZ27_AA")
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001795586205"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", None)
 
 
